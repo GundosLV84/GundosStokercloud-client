f09fef91aa83279d080c46dc5ffaae66df734f19
GundosLV84 <155369559+GundosLV84@users.noreply.github.com>
2024-03-30 00:19:20 +0200

    test

--- src/GundosStokercloud/client.py
+++ src/GundosStokercloud/client.py
@@ -18,7 +18,8 @@ class TokenInvalid(Exception):
 class Client:
     BASE_URL = "http://www.stokercloud.dk/"
     QUERY_SCREEN = "b1,17,b2,5,b3,4,b4,6,b5,12,b6,3,b7,7,b8,24,b9,25,b10,9,d1,3,d2,4,d3,4,d4,4,d5,4,d6,4,d7,0,d8,0,d9,0,d10,0,h1,2,h2,3,h3,4,h4,7,h5,8,h6,10,h7,9,h8,2,h9,15,h10,3,w1,2,w2,3,w3,9,w4,0,w5,0"
-    query_url = ""
+    query_url = None
+
     def __init__(self, name: str, password: str = None, cache_time_seconds: int = 10):
         self.name = name
         self.password = password
